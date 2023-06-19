3cfb55d0e813ec248a2e60b88f16a63048fb0c76
TUSHAR KANT SAO <77832400+Tushar-2510@users.noreply.github.com>
2023-06-19 15:36:50 +0530

    Update spotify.sh

--- spotify.sh
+++ spotify.sh
@@ -4,7 +4,7 @@
 
 access="$(curl -X POST "https://accounts.spotify.com/api/token" \
      -H "Content-Type: application/x-www-form-urlencoded" \
-     -d "grant_type=client_credentials&client_id=a07a807ea6bc47499ab66177cd36e371&client_secret=b232e2a7da4c4bc79e6cb38a15877b1c" \
+     -d "grant_type=client_credentials&client_id=${{ secrets.CLIENT_ID }}&client_secret=${{ secrets.CLIENT_SECRET}}" \
      | jq -r '. | .access_token')"  
 # echo $access
 # curl "https://api.spotify.com/v1/artists/4Z8W4fKeB5YxbusRsdQVPb" \
